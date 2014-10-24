4411696a5d1969c24407a54aaca4c50fa81a4baf
Laci <Laci@Laci-PC>
2014-10-24 20:16:14 +0200

    pdo generalas oké

--- PDOData.java
+++ PDOData.java
@@ -5,7 +5,15 @@ public class PDOData extends Data {
 	private String theme;
 	private String function;
 	private String subtheme;
-	
+	//inputok
+	private float blocksize;
+	private float recordsize;
+	private float recordnumber;
+	private float keysize;
+	private float pointersize;
+	private float bucketsize;
+	private float blockoperationtime;
+	private float freememory;
 	
 	public PDOData(){}
 	
@@ -20,6 +28,82 @@ public class PDOData extends Data {
 	public String[] getJobCommand (){
 		return jobcommand;
 	}
+	
+	public String getTheme (){
+		return theme;
+	}
+	
+	public String getFunction (){
+		return function;
+	}
+	
+	public String getSubTheme (){
+		return subtheme;
+	}
+	
+	public void setBlockSize (float blo){
+		blocksize = blo;
+	}
+	
+	public float getBlockSize (){
+		return blocksize;
+	}
+	
+	public void setRecordSize (float rec){
+		recordsize = rec ;
+	}
+	
+	public float getRecordSize (){
+		return recordsize;
+	}
+	
+	public void setRecordNumber (float rec ){
+		recordnumber  = rec;
+	}
+	
+	public float getRecordNumber (){
+		return recordnumber;
+	}
+	
+	public void setKeySize (float key){
+		 keysize = key ;
+	}
+	
+	public float getKeySize (){
+		return keysize;
+	}
+	
+	public void setPointerSize (float poi){
+		pointersize = poi;
+	}
+	
+	public float getPointerSize (){
+		return pointersize;
+	}
+	
+	public void setBucketSize (float buc){
+		bucketsize = buc;
+	}
+	
+	public float getBucketSize (){
+		return bucketsize;
+	}
+	public void setBlockOperationTime (float blo){
+		blockoperationtime = blo;
+	}
+	
+	public float getBlockOperationTime (){
+		return blockoperationtime;
+	}
+	
+	public void setFreeMemory (float fre){
+		freememory = fre;
+	}
+	
+	public float getFreeMemory (){
+		return freememory;
+	}
+	
 	
 	
 }
--- PDOEngine.java
+++ PDOEngine.java
@@ -1,14 +1,57 @@
+import java.util.Random;
+
 
 public class PDOEngine extends Engine {
-	Serialization pdoser = new Serialization ();
-	int a = 0;
+	Serialization pdoser;
 	PDOData pdodata;
+	
 	public PDOEngine (){
-		pdodata = new PDOData ();	
+		pdodata = new PDOData ();
+		pdoser = new Serialization ();
 	}
 	
 	public void generateData (String[] feladat){
 		pdodata.setJobCommand(feladat);
+		
+		Random myrandom = new Random();
+		float randomfloat =(float) myrandom.nextInt(9000)+1000;
+		System.out.println(randomfloat);
+		pdodata.setBlockSize(randomfloat);
+		
+		
+		float randomrecordfloat =(float) myrandom.nextInt((int) randomfloat- 100)+100;
+		System.out.println(randomrecordfloat);
+		pdodata.setRecordSize(randomrecordfloat);
+		
+		float randomrecordnumberfloat =(float) myrandom.nextInt(999999)+1;
+		System.out.println(randomrecordnumberfloat);
+		pdodata.setRecordNumber(randomrecordnumberfloat);
+		
+		float randomkeyfloat =(float) myrandom.nextInt(480)+20;
+		System.out.println(randomkeyfloat);
+		pdodata.setKeySize(randomkeyfloat);
+		
+		float randompointerfloat =(float) myrandom.nextInt(1016)+8;
+		System.out.println(randompointerfloat);
+		pdodata.setPointerSize(randompointerfloat);
+		
+		float randombucketfloat =(float) (myrandom.nextInt(50)*randomrecordfloat) + myrandom.nextInt((int)randomrecordfloat);
+		System.out.println(randombucketfloat);
+		pdodata.setBucketSize(randombucketfloat);
+		
+		float randomblockopfloat =(float) myrandom.nextInt(49)+1;
+		System.out.println(randomblockopfloat);
+		pdodata.setBlockOperationTime (randomblockopfloat);
+		
+		float randomfreememoryfloat =(float) myrandom.nextInt((int) randomrecordfloat) + randomrecordfloat;
+		System.out.println(randomfreememoryfloat);
+		pdodata.setFreeMemory (randomfreememoryfloat);
+		
+		
+		
+		
+		
+		
 	}
 	//mentés metódus
 	public void saveMethod(){
